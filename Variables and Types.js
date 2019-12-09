2d886b4ce2ef0bd4438c415e773b5bd1efc8d5cf
Ryan Torske <rtorske@cnm.edu>
2019-12-09 12:53:56 -0700

    Fundamentals practice complete

---
+++ Variables and Types.js
@@ -0,0 +1,46 @@
+/*
+ *Variables, Data Types, and Typing
+ */
+let wordData;
+
+wordData = "Word list";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = x * 0.07;
+let totalwithtax = tax + subTotal;
+
+
+//DATA TYPES
+let obj = {
+	key1: "value",
+	key2: 4,
+	boolean: true,
+	obj2: {
+		obj2key1: "Internal Object Value",
+	},
+};
+console.log(obj.obj2.obj2key1);
+
+
+//TYPING SYSTEMS
+let variable = "string";
+
+let var1 = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result)
+
+//OPERATORS
+let add = 1 + 2;
+let sub = 2 - 1;
+let mult = 2 * 4;
+let division = 4 / 2;
+let mod = 5 % 2;
+
+//STRINGS
+let string = "String 1 " + "String 2";
+
+console.log(string);
